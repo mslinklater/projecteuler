066597ca7b17569b851454e6ad0f235f750242d8
Martin Linklater <mslinklater@gmail.com>
2017-12-22 21:54:33 +0000

    Added outline for problem 4 and slight edit to problem 3

---
+++ problem4/main.go
@@ -0,0 +1,19 @@
+/*
+Problem 4 - Largest palindrome product
+
+A palindromic number reads the same both ways. The largest palindrome made from
+the product of two 2-digit numbers is 9009 = 91x99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Problem 4 - Largest palindrome product")
+}
+
+func isPalindromic(num int) bool {
+	return false
+}
